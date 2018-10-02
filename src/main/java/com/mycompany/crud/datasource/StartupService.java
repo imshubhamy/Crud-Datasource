5489b633712f23ac75eadf8557b8740d4c21c61a
imshubhamy <imshubhamy@gmail.com>
2018-10-02 13:08:26 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/crud/datasource/StartupService.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.crud.datasource;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import static java.lang.System.out;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.servlet.*;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author Admin
+ */
+
+
+@Startup
+@Singleton
+public class StartupService {
+     
+    @Resource(lookup="java:comp/DefaultDataSource")
+    private DataSource dataSource;
+ 
+    @PostConstruct
+    public void init() {
+        try (Connection connection = dataSource.getConnection()) {
+            out.println(
+                connection.getMetaData().getDatabaseProductName() + "-" +
+                connection.getCatalog()
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+     
+}
+
